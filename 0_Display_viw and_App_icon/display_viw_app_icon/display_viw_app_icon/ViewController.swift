b083c754c9f3b634df2aa75e99ff96a2f46a979a
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2025-01-24 00:36:27 +0600

    Add files via upload

---
+++ 0_Display_viw and_App_icon/display_viw_app_icon/display_viw_app_icon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  display_viw_app_icon
+//
+//  Created by TwinBit Ltd on 29/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 0_Display_viw and_App_icon/display_viw_app_icon/display_viw_app_iconTests/display_viw_app_iconTests.swift
@@ -0,0 +1,17 @@
+//
+//  display_viw_app_iconTests.swift
+//  display_viw_app_iconTests
+//
+//  Created by TwinBit Ltd on 29/12/24.
+//
+
+import Testing
+@testable import display_viw_app_icon
+
+struct display_viw_app_iconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
