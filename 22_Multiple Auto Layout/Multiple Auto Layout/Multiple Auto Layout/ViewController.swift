bf2e11d89c743353f31de331b3720effd767eec0
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2025-01-24 00:44:29 +0600

    Add files via upload

---
+++ 22_Multiple Auto Layout/Multiple Auto Layout/Multiple Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiple Auto Layout
+//
+//  Created by Alimullah on 23/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 22_Multiple Auto Layout/Multiple Auto Layout/Multiple Auto LayoutTests/Multiple_Auto_LayoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  Multiple_Auto_LayoutTests.swift
+//  Multiple Auto LayoutTests
+//
+//  Created by Alimullah on 23/1/25.
+//
+
+import Testing
+@testable import Multiple_Auto_Layout
+
+struct Multiple_Auto_LayoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
