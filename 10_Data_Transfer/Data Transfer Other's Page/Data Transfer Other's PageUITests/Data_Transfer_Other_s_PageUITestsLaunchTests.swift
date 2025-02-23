af5f10013960a53cbc35e90c80ebdf8bb2649899
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2025-02-23 09:05:35 +0600

    Add files via upload

---
+++ 10_Data_Transfer/Data Transfer Other's Page/Data Transfer Other's PageUITests/Data_Transfer_Other_s_PageUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Data_Transfer_Other_s_PageUITestsLaunchTests.swift
+//  Data Transfer Other's PageUITests
+//
+//  Created by TwinBit Ltd on 4/1/25.
+//
+
+import XCTest
+
+final class Data_Transfer_Other_s_PageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
