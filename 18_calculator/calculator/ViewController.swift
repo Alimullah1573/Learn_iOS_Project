1509a73d4e8aebc397743427e5c36c135b6a8b22
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2025-02-23 09:14:31 +0600

    Add files via upload

---
+++ 18_calculator/calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Alimullah on 19/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
