4b6f3bcead3106d2f94b1eb0278a7a3951504caa
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2025-02-23 09:00:07 +0600

    Add files via upload

---
+++ 4_Multiple_Page/Multiple Page/Multiple Page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiple Page
+//
+//  Created by TwinBit Ltd on 3/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4_Multiple_Page/Multiple Page/Multiple PageTests/Multiple_PageTests.swift
@@ -0,0 +1,17 @@
+//
+//  Multiple_PageTests.swift
+//  Multiple PageTests
+//
+//  Created by TwinBit Ltd on 3/1/25.
+//
+
+import Testing
+@testable import Multiple_Page
+
+struct Multiple_PageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
