e3a45d1c0202bb4ea257b44cf5424e61f15dcd9f
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2025-02-23 09:11:14 +0600

    Add files via upload

---
+++ 13_Navigation Controller/Navigation Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation Controller
+//
+//  Created by TwinBit Ltd on 12/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 13_Navigation Controller/Navigation ControllerTests/Navigation_ControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_ControllerTests.swift
+//  Navigation ControllerTests
+//
+//  Created by TwinBit Ltd on 12/1/25.
+//
+
+import Testing
+@testable import Navigation_Controller
+
+struct Navigation_ControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
